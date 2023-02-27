9e44aa008214955dcf590e87e5d12efcf8dff6eb
Dario Küffer <dariokueffer@icloud.com>
2023-02-27 13:53:03 +0100

    removed name attribute

--- src/main/java/ch/uzh/ifi/hase/soprafs23/entity/User.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/entity/User.java
@@ -25,9 +25,6 @@ public class User implements Serializable {
   @GeneratedValue
   private Long id;
 
-  @Column(nullable = false)
-  private String name;
-
   @Column(nullable = false, unique = true)
   private String username;
 
@@ -43,14 +40,6 @@ public class User implements Serializable {
 
   public void setId(Long id) {
     this.id = id;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
   }
 
   public String getUsername() {
--- src/main/java/ch/uzh/ifi/hase/soprafs23/service/UserService.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/service/UserService.java
@@ -64,16 +64,11 @@ public class UserService {
    */
   private void checkIfUserExists(User userToBeCreated) {
     User userByUsername = userRepository.findByUsername(userToBeCreated.getUsername());
-    User userByName = userRepository.findByName(userToBeCreated.getName());
 
     String baseErrorMessage = "The %s provided %s not unique. Therefore, the user could not be created!";
-    if (userByUsername != null && userByName != null) {
-      throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
-          String.format(baseErrorMessage, "username and the name", "are"));
-    } else if (userByUsername != null) {
-      throw new ResponseStatusException(HttpStatus.BAD_REQUEST, String.format(baseErrorMessage, "username", "is"));
-    } else if (userByName != null) {
-      throw new ResponseStatusException(HttpStatus.BAD_REQUEST, String.format(baseErrorMessage, "name", "is"));
+    if (userByUsername != null) {
+      throw new ResponseStatusException(HttpStatus.CONFLICT,
+          String.format(baseErrorMessage, "username", "is"));
     }
   }
 }
