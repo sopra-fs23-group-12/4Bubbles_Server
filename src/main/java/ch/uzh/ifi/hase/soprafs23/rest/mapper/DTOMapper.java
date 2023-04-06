7b917243f88fa429c3f334def237ed974aa03dda
fabibert <76185888+fabibert@users.noreply.github.com>
2023-04-06 16:16:23 +0200

    fix merge mistake

--- src/main/java/ch/uzh/ifi/hase/soprafs23/rest/mapper/DTOMapper.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/rest/mapper/DTOMapper.java
@@ -20,7 +20,7 @@ import org.mapstruct.factory.Mappers;
  */
 @Mapper
 public interface DTOMapper {
-    
+
     DTOMapper INSTANCE = Mappers.getMapper(DTOMapper.class);
 
     @Mapping(source = "leaderId", target = "leaderUserId")
