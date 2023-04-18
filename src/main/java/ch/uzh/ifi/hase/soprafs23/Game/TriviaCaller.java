fe2b29ae0cee0692f3ee0e7be6f2595b837d40b8
Loui <devillers.louis@outlook.com>
2023-04-18 18:57:42 +0200

    Revert "Merge pull request #62 from sopra-fs23-group-12/GameLogic"
    This reverts commit 1b8e873f2eafc10f6a3c54d624052e629c1cb7a3, reversing
    changes made to ff7e1d4d64849258b0cf25e0c939555ed70a6e86.

--- src/main/java/ch/uzh/ifi/hase/soprafs23/Game/TriviaCaller.java
+++
@@ -1,9 +0,0 @@
-package ch.uzh.ifi.hase.soprafs23.Game;
-
-import ch.uzh.ifi.hase.soprafs23.Game.stateStorage.Question;
-
-import java.util.List;
-
-public interface TriviaCaller {
-    public List<Question> getTriviaQuestions(int numOfQuestions, String questionTopic);
-}
--- src/main/java/ch/uzh/ifi/hase/soprafs23/Game/stateStorage/TimerCallBack.java
+++
@@ -1,5 +0,0 @@
-package ch.uzh.ifi.hase.soprafs23.Game.stateStorage;
-
-public interface TimerCallBack {
-    void onElapsed(int elapsedTimeInSeconds);
-}
