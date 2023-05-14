e865296eca4459c3795459e69ede3fe1eae955c8
fabibert <76185888+fabibert@users.noreply.github.com>
2023-05-14 16:15:58 +0200

    testing exceptions for minor improvements

---
+++ src/test/java/ch/uzh/ifi/hase/soprafs23/Entity/ExceptionUnitTests.java
@@ -0,0 +1,518 @@
+package ch.uzh.ifi.hase.soprafs23.Entity;
+
+import ch.uzh.ifi.hase.soprafs23.ServerCommandLineRunner;
+import ch.uzh.ifi.hase.soprafs23.exceptions.GlobalExceptionAdvice;
+import ch.uzh.ifi.hase.soprafs23.exceptions.RoomNotFoundException;
+import com.corundumstudio.socketio.Configuration;
+import com.corundumstudio.socketio.SocketIOServer;
+import org.junit.jupiter.api.Test;
+import org.springframework.web.context.request.WebRequest;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.security.Principal;
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class ExceptionUnitTests {
+
+
+    @Test
+    public void testRoomNotFoundException() throws Exception {
+        RoomNotFoundException roomNotFoundException = new RoomNotFoundException("room with code 123456 not found", new Throwable());
+    }
+
+    @Test
+    public void testServerCommandLineRunner() throws Exception {
+        ServerCommandLineRunner serverCommandLineRunner = new ServerCommandLineRunner(new SocketIOServer(new Configuration()));
+        assertThrows(Exception.class, () -> serverCommandLineRunner.run());
+    }
+
+
+
+    @Test
+    public void testHandleConflict(){
+        GlobalExceptionAdvice globalExceptionAdvice = new GlobalExceptionAdvice();
+        globalExceptionAdvice.handleTransactionSystemException(new Exception(), new HttpServletRequest() {
+            @Override
+            public String getAuthType() {
+                return null;
+            }
+
+            @Override
+            public Cookie[] getCookies() {
+                return new Cookie[0];
+            }
+
+            @Override
+            public long getDateHeader(String name) {
+                return 0;
+            }
+
+            @Override
+            public String getHeader(String name) {
+                return null;
+            }
+
+            @Override
+            public Enumeration<String> getHeaders(String name) {
+                return null;
+            }
+
+            @Override
+            public Enumeration<String> getHeaderNames() {
+                return null;
+            }
+
+            @Override
+            public int getIntHeader(String name) {
+                return 0;
+            }
+
+            @Override
+            public String getMethod() {
+                return null;
+            }
+
+            @Override
+            public String getPathInfo() {
+                return null;
+            }
+
+            @Override
+            public String getPathTranslated() {
+                return null;
+            }
+
+            @Override
+            public String getContextPath() {
+                return null;
+            }
+
+            @Override
+            public String getQueryString() {
+                return null;
+            }
+
+            @Override
+            public String getRemoteUser() {
+                return null;
+            }
+
+            @Override
+            public boolean isUserInRole(String role) {
+                return false;
+            }
+
+            @Override
+            public Principal getUserPrincipal() {
+                return null;
+            }
+
+            @Override
+            public String getRequestedSessionId() {
+                return null;
+            }
+
+            @Override
+            public String getRequestURI() {
+                return null;
+            }
+
+            @Override
+            public StringBuffer getRequestURL() {
+                return null;
+            }
+
+            @Override
+            public String getServletPath() {
+                return null;
+            }
+
+            @Override
+            public HttpSession getSession(boolean create) {
+                return null;
+            }
+
+            @Override
+            public HttpSession getSession() {
+                return null;
+            }
+
+            @Override
+            public String changeSessionId() {
+                return null;
+            }
+
+            @Override
+            public boolean isRequestedSessionIdValid() {
+                return false;
+            }
+
+            @Override
+            public boolean isRequestedSessionIdFromCookie() {
+                return false;
+            }
+
+            @Override
+            public boolean isRequestedSessionIdFromURL() {
+                return false;
+            }
+
+            @Override
+            public boolean isRequestedSessionIdFromUrl() {
+                return false;
+            }
+
+            @Override
+            public boolean authenticate(HttpServletResponse response) throws IOException, ServletException {
+                return false;
+            }
+
+            @Override
+            public void login(String username, String password) throws ServletException {
+
+            }
+
+            @Override
+            public void logout() throws ServletException {
+
+            }
+
+            @Override
+            public Collection<Part> getParts() throws IOException, ServletException {
+                return null;
+            }
+
+            @Override
+            public Part getPart(String name) throws IOException, ServletException {
+                return null;
+            }
+
+            @Override
+            public <T extends HttpUpgradeHandler> T upgrade(Class<T> httpUpgradeHandlerClass) throws IOException, ServletException {
+                return null;
+            }
+
+            @Override
+            public Object getAttribute(String name) {
+                return null;
+            }
+
+            @Override
+            public Enumeration<String> getAttributeNames() {
+                return null;
+            }
+
+            @Override
+            public String getCharacterEncoding() {
+                return null;
+            }
+
+            @Override
+            public void setCharacterEncoding(String env) throws UnsupportedEncodingException {
+
+            }
+
+            @Override
+            public int getContentLength() {
+                return 0;
+            }
+
+            @Override
+            public long getContentLengthLong() {
+                return 0;
+            }
+
+            @Override
+            public String getContentType() {
+                return null;
+            }
+
+            @Override
+            public ServletInputStream getInputStream() throws IOException {
+                return null;
+            }
+
+            @Override
+            public String getParameter(String name) {
+                return null;
+            }
+
+            @Override
+            public Enumeration<String> getParameterNames() {
+                return null;
+            }
+
+            @Override
+            public String[] getParameterValues(String name) {
+                return new String[0];
+            }
+
+            @Override
+            public Map<String, String[]> getParameterMap() {
+                return null;
+            }
+
+            @Override
+            public String getProtocol() {
+                return null;
+            }
+
+            @Override
+            public String getScheme() {
+                return null;
+            }
+
+            @Override
+            public String getServerName() {
+                return null;
+            }
+
+            @Override
+            public int getServerPort() {
+                return 0;
+            }
+
+            @Override
+            public BufferedReader getReader() throws IOException {
+                return null;
+            }
+
+            @Override
+            public String getRemoteAddr() {
+                return null;
+            }
+
+            @Override
+            public String getRemoteHost() {
+                return null;
+            }
+
+            @Override
+            public void setAttribute(String name, Object o) {
+
+            }
+
+            @Override
+            public void removeAttribute(String name) {
+
+            }
+
+            @Override
+            public Locale getLocale() {
+                return null;
+            }
+
+            @Override
+            public Enumeration<Locale> getLocales() {
+                return null;
+            }
+
+            @Override
+            public boolean isSecure() {
+                return false;
+            }
+
+            @Override
+            public RequestDispatcher getRequestDispatcher(String path) {
+                return null;
+            }
+
+            @Override
+            public String getRealPath(String path) {
+                return null;
+            }
+
+            @Override
+            public int getRemotePort() {
+                return 0;
+            }
+
+            @Override
+            public String getLocalName() {
+                return null;
+            }
+
+            @Override
+            public String getLocalAddr() {
+                return null;
+            }
+
+            @Override
+            public int getLocalPort() {
+                return 0;
+            }
+
+            @Override
+            public ServletContext getServletContext() {
+                return null;
+            }
+
+            @Override
+            public AsyncContext startAsync() throws IllegalStateException {
+                return null;
+            }
+
+            @Override
+            public AsyncContext startAsync(ServletRequest servletRequest, ServletResponse servletResponse) throws IllegalStateException {
+                return null;
+            }
+
+            @Override
+            public boolean isAsyncStarted() {
+                return false;
+            }
+
+            @Override
+            public boolean isAsyncSupported() {
+                return false;
+            }
+
+            @Override
+            public AsyncContext getAsyncContext() {
+                return null;
+            }
+
+            @Override
+            public DispatcherType getDispatcherType() {
+                return null;
+            }
+        });
+    }
+
+    @Test
+    public void testHandleException() throws Exception {
+        GlobalExceptionAdvice globalExceptionAdvice = new GlobalExceptionAdvice();
+        assertThrows(Exception.class, () -> globalExceptionAdvice.handleException(new Exception(), new WebRequest() {
+            @Override
+            public String getHeader(String headerName) {
+                return null;
+            }
+
+            @Override
+            public String[] getHeaderValues(String headerName) {
+                return new String[0];
+            }
+
+            @Override
+            public Iterator<String> getHeaderNames() {
+                return null;
+            }
+
+            @Override
+            public String getParameter(String paramName) {
+                return null;
+            }
+
+            @Override
+            public String[] getParameterValues(String paramName) {
+                return new String[0];
+            }
+
+            @Override
+            public Iterator<String> getParameterNames() {
+                return null;
+            }
+
+            @Override
+            public Map<String, String[]> getParameterMap() {
+                return null;
+            }
+
+            @Override
+            public Locale getLocale() {
+                return null;
+            }
+
+            @Override
+            public String getContextPath() {
+                return null;
+            }
+
+            @Override
+            public String getRemoteUser() {
+                return null;
+            }
+
+            @Override
+            public Principal getUserPrincipal() {
+                return null;
+            }
+
+            @Override
+            public boolean isUserInRole(String role) {
+                return false;
+            }
+
+            @Override
+            public boolean isSecure() {
+                return false;
+            }
+
+            @Override
+            public boolean checkNotModified(long lastModifiedTimestamp) {
+                return false;
+            }
+
+            @Override
+            public boolean checkNotModified(String etag) {
+                return false;
+            }
+
+            @Override
+            public boolean checkNotModified(String etag, long lastModifiedTimestamp) {
+                return false;
+            }
+
+            @Override
+            public String getDescription(boolean includeClientInfo) {
+                return null;
+            }
+
+            @Override
+            public Object getAttribute(String name, int scope) {
+                return null;
+            }
+
+            @Override
+            public void setAttribute(String name, Object value, int scope) {
+
+            }
+
+            @Override
+            public void removeAttribute(String name, int scope) {
+
+            }
+
+            @Override
+            public String[] getAttributeNames(int scope) {
+                return new String[0];
+            }
+
+            @Override
+            public void registerDestructionCallback(String name, Runnable callback, int scope) {
+
+            }
+
+            @Override
+            public Object resolveReference(String key) {
+                return null;
+            }
+
+            @Override
+            public String getSessionId() {
+                return null;
+            }
+
+            @Override
+            public Object getSessionMutex() {
+                return null;
+            }
+        }));
+    }
+}
